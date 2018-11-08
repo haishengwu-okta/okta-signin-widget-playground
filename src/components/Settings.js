14feb4c62fc09ef04b9451bc8f449abd268822af
Haisheng Wu <haisheng.wu@okta.com>
2018-11-08 13:07:52 -0800

    refacter the setting ui.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import SettingsConfig from './SettingsConfig';
 import PropTypes from 'prop-types';
 import './Settings.css';
-import { Header, List, Checkbox } from 'semantic-ui-react'
+import { Header, List, Checkbox, Popup, Divider, Form } from 'semantic-ui-react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const checkboxItems = SettingsConfig.features;
@@ -22,11 +23,7 @@ class Settings extends Component {
   };
 
   notify = (message) => {
-   toast.info(message);
-  }
-
-  constructor(opt) {
-    super(opt);
+    toast.info(message);
   }
 
   componentDidMount() {
@@ -39,52 +36,71 @@ class Settings extends Component {
     this.notify("Widget Config Updated");
   }
 
+  updateSignInWidget = _.throttle(() => {
+    this.props.settingChangedFn(signInWidgetOption);
+    this.notify("SignIn Widget option has been updated.");
+  }, 5000);
+
   updateBaseUrl = (event) => {
     const inputValue = event.currentTarget.value;
-    if (inputValue) {
-      const self = this;
-      // fake debounce
-      setTimeout(function() {
+    if (inputValue.indexOf('http') === 0) {
         signInWidgetOption.baseUrl = inputValue;
-        self.props.settingChangedFn(signInWidgetOption);
-        self.notify("Base URL updated");
-      }, 4000);
-
+        this.updateSignInWidget();
     }
   }
+
+  updateStateToken = (event) => {
+    const inputValue = event.currentTarget.value;
+    signInWidgetOption.stateToken = inputValue;
+    this.updateSignInWidget();
+  };
 
   render() {
     return (
       <div className="Settings">
         <Header as='h2'>Widget Settings</Header>
 
-        <Header as='h3'>Features</Header>
-        <List>
-          {
-            checkboxItems.map((label) => {
-              return <List.Item key={label}>
-                       <Checkbox label={label} onChange={this.toggleCheckbox} />
-                     </List.Item>
-            })
-          }
-        </List>
-        <div className='ui divider' />
-        <p>Specify a base url to bootstrap the widget</p>
-        <div className='ui labeled input'>
-          <div className='ui label label'> </div>
-          <input type="text" onChange={this.updateBaseUrl} placeholder="oktadomain url"/>
-          <ToastContainer
-            position="top-center"
-            autoClose={1000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnVisibilityChange
-            draggable
-            pauseOnHover
-          />
-        </div>
+        <Form size="small">
+
+          <Header as='h3'>Features</Header>
+          <Form.Field>
+            <List>
+              {
+                checkboxItems.map((label) => {
+                  return <List.Item key={label}>
+                    <Checkbox label={label} onChange={this.toggleCheckbox} />
+                  </List.Item>
+                })
+              }
+            </List>
+          </Form.Field>
+
+          <Divider />
+
+          <Form.Field>
+            <Popup trigger={<Form.Input label="Base URL" onBlur={this.updateBaseUrl} placeholder="okta domain url"/> }>
+              Specify a base url to bootstrap the widget
+            </Popup>
+          </Form.Field>
+
+          <Form.Field>
+            <Form.Input label="State Token" placeholder="" onBlur={this.updateStateToken} />
+          </Form.Field>
+
+        </Form>
+
+        <ToastContainer
+          position="top-center"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          draggable
+          pauseOnHover
+        />
+
       </div>
     );
   }
