d666ef2a8fadb3d6387bc101025ef9a1fe7c5106
Haisheng Wu <haisheng.wu@okta.com>
2018-12-13 10:01:48 -0800

    adjust footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Label } from 'semantic-ui-react';
 import './Footer.css';
 
 class Footer extends React.Component {
   render() {
   return (
-    <Card className="footer">
+    <Label className="footer">
       <a href="http://www.okta.com">Powered by Okta @ 2018</a>
-    </Card>
+    </Label>
     )
   }
 }
